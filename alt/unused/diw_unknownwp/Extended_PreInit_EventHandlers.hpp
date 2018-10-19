a956aa9bf32198e54fcccb5bba85b27dc8d47952
G3rrus <bot321@gmx.at>
2018-10-19 13:29:08 +0200

    removed some old scripts

--- alt/unused/diw_unknownwp/Extended_PreInit_EventHandlers.hpp
+++
@@ -1,3 +0,0 @@
-class diw_unknownwp_settings {
-  init = "call compile preprocessFileLineNumbers 'diw\diw_unknownwp\fn_addCbaSettings.sqf'";
-};
--- alt/unused/diw_unknownwp/cfgFunctions.hpp
+++
@@ -1,7 +0,0 @@
-class diw_unknownwp {
-  tag="diw_unknownwp";
-  class functions {
-    file = "diw\diw_unknownwp";
-    class init {postInit = 1;};
-  };
-};
